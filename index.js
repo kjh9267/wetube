5ad91941b5145c1b12257e6ec2a5e03f601aa13b
kjh9267 <intellijunho@kakao.com>
2019-07-12 20:16:07 +0900

    route

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ const PORT = 4000;
 function handleListening(){
 	console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req, res){
+	res.send('hi from home');
+}
+
+function handleProfile(req, res){
+	res.send('you are in my profile');
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
