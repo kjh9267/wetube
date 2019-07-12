e7763511983621e43c31a9a53f3d8ae8ef2225b6
kjh9267 <kjh9267z@naver.com>
2019-07-12 21:12:12 +0900

    nodemon & babel

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
-function handleListening(){
-	console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res){
-	res.send('hi from home');
-}
+const handleHome = (req, res) => res.send('hi from home');
 
-function handleProfile(req, res){
-	res.send('you are in my profile');
-}
+const handleProfile = (req, res) => res.send('you are in my profile');
 
 app.get("/", handleHome);
 
