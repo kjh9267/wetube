00076a0746c244be0b2d8856b424916da536b034
kjh9267 <kjh9267z@naver.com>
2019-07-13 19:25:05 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,12 +13,11 @@ const handleHome = (req, res) => res.send('hi from home');
 
 const handleProfile = (req, res) => res.send('you are in my profile');
 
-const between = (req, res, next) =>{
-	console.log("I'm between");
-	next();
-}
-
-app.use(between);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
