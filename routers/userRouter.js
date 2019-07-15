804715f8340be5626764382c8a180726e7c5dee2
kjh9267 <intellijunho@kakao.com>
2019-07-15 22:21:36 +0900

    mvc

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from '../routes';
+import {changePassword, editProfile, userDetail, users} from "../controllers/userController";
 
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('Users'));
-userRouter.get(routes.userDetail, (req, res) => res.send('Users Detail'));
-userRouter.get(routes.editProfile, (req, res) => res.send('Edit Profile'));
-userRouter.get(routes.changePassword, (req, res) => res.send('change Password'));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
