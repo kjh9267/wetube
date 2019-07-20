7e8bfd590714425580d6e2a15a39defabb2a3ae0
kjh9267 <intellijunho@kakao.com>
2019-07-20 16:51:34 +0900

    Videos_model

--- init.js
+++ init.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const PORT = process.env.PORT ||  4000;
+const PORT = process.env.PORT || 4000;
 
 
 const handleListening = () => console.log(`✅  Listening on: http://localhost:${PORT}`);
