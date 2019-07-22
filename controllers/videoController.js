83a81f2ac1c985d9833a4ebf4186971c9b228d55
kjh9267 <intellijunho@kakao.com>
2019-07-22 20:38:36 +0900

    edit_video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -64,7 +64,7 @@ export const postEditVideo = async (req, res) => {
         body: { title, description }
     } = req;
     try {
-        await Video.findOneAndUpdate({ id }, { title, description });
+        await Video.findOneAndUpdate({ _id: id }, { title, description });
         res.redirect(routes.videoDetail(id));
     } catch (error) {
         res.redirect(routes.home);
